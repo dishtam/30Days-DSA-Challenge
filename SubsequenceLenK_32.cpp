61c7f31765dc76defb531eb5d20f177f4a9e4db8
Rohit Roy <97011184+dishtam@users.noreply.github.com>
2024-01-11 19:43:38 +0530

    Create SubsequenceLenK_32.cpp
    Sorted the element based on weight first then based on indexes.

---
+++ SubsequenceLenK_32.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> maxSubsequence(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<pair<int,int>> v(n);
+        vector<int> ans(k);
+
+        for(int i=0;i<n;i++){
+            v[i] = {nums[i],i};
+        }
+
+        sort(v.rbegin(),v.rend());
+        sort(v.begin(),v.begin()+k,[](auto a,auto b){
+            return a.second<b.second;
+        });
+
+        for(int i=0;i<k;i++){
+            ans[i]=v[i].first;
+        }
+        return ans;
+    }
+};
